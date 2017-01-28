5ec583577f3b52bdaa8e985ea66326e773be609d
Jeremiah Boyle <jeremiah.boyle@gmail.com>
2017-01-28 00:58:32 -0800

    Use blueprint to expose main views

--- app/app.py
+++ app/app.py
@@ -3,15 +3,13 @@ import os
 from flask import Flask
 
 from database import configure_db
-from views import create_views
+from views import main as main_blueprint
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+    app.register_blueprint(main_blueprint)
     configure_db(app)
-    create_views(app)
-
     return app
