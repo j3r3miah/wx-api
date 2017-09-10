de68968305dbf93d8e48f948725397158331fa16
Jeremiah Boyle <jeremiah.boyle@gmail.com>
2017-09-10 00:32:06 -0700

    Move scraper stuff out of flask app (wx-api). Needs to integrate with service.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,9 @@
 import os
 import json
-import atexit
 import logging
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-from app.scraper import Scraper
 
 logging.basicConfig(
     level=logging.INFO,
@@ -22,32 +19,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-_scraper = None
-
-def _scraper_quit():
-    if _scraper:
-        log.info('destroying scraper')
-        _scraper.shutdown()
-
-atexit.register(_scraper_quit)
-
-
 def create_app():
     from .views import main as main_blueprint
     app.register_blueprint(main_blueprint)
     return app
-
-def scraper():
-   return _scraper
-
-def recreate_scraper():
-    global _scraper
-    _scraper_quit()
-    log.info('creating scraper')
-    _scraper = Scraper(
-        app.config['CHROME_DRIVER_PATH'],
-        app.config['SCRAPER_DATA_DIR'],
-        json.load(open(app.config['WEBSITE_URLS']))
-    )
-
-recreate_scraper()
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from flask import Blueprint, jsonify, render_template, request
 
 from app.models import User
 from app.schemas import UserSchema
-from app import app, db, scraper, recreate_scraper
+from app import app, db
 
 main = Blueprint('main', __name__)
 
@@ -19,17 +19,12 @@ def hello():
 
 @main.route('/reset/')
 def reset():
-    recreate_scraper()
     return jsonify(True)
 
 @main.route('/login/')
 def login():
-    creds = json.load(open(app.config['WEBSITE_CREDENTIALS']))
-    success = scraper().login(creds['username'], creds['password'])
-    return jsonify(success)
+    return jsonify(True)
 
 @main.route('/spot/')
 def spot():
-    # TODO test code
-    data = scraper().get_spot_data(1786)
-    return jsonify(data)
+    return jsonify({})
